636ac8f17c007011e8d0a7a2e71cd58ea52d6cf3
trevor-henderson <trevorsgit@gmail.com>
2017-04-01 16:34:15 -0500

    configured resource service

--- smartsync-auth-service/src/main/java/com/smartsync/config/ResourceServerConfiguration.java
+++ smartsync-auth-service/src/main/java/com/smartsync/config/ResourceServerConfiguration.java
@@ -1,13 +1,32 @@
 package com.smartsync.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
  * Created by trev on 4/1/17.
  */
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfiguration {
-    
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    /**
+     *
+     */
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.requestMatchers().antMatchers("/auth")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth").access("#oath2.hasScope('read')");
+    }
+
+
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("apis");
+    }
+
 }
