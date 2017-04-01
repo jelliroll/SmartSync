4a5e7e74f819bbc77cb20f64809a93a4de3c86b7
trevor-henderson <trevorsgit@gmail.com>
2017-04-01 17:31:19 -0500

    auth-service still need initilizer

--- smartsync-auth-service/src/main/java/com/smartsync/SmartsyncAuthServiceApplication.java
+++ smartsync-auth-service/src/main/java/com/smartsync/SmartsyncAuthServiceApplication.java
@@ -1,8 +1,11 @@
 package com.smartsync;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @EnableEurekaClient
--- smartsync-auth-service/src/main/java/com/smartsync/config/OAuthConfiguration.java
+++ smartsync-auth-service/src/main/java/com/smartsync/config/OAuthConfiguration.java
@@ -1,10 +1,21 @@
 package com.smartsync.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * Created by trev on 4/1/17.
@@ -15,6 +26,50 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private AuthenticationManager auth;
+
+    @Autowired
+    private DataSource dataSource;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Bean
+    public JdbcTokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    protected AuthorizationCodeServices authorizationCodeServices() {
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security)
+            throws Exception {
+        security.passwordEncoder(passwordEncoder);
+    }
+
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authorizationCodeServices(authorizationCodeServices())
+                .authenticationManager(auth).tokenStore(tokenStore())
+                .approvalStoreDisabled();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)  throws Exception{
+        clients.jdbc(dataSource)
+                .passwordEncoder(passwordEncoder)
+                .withClient("client")
+                .authorizedGrantTypes("authorization_code","client_credentials",
+                        "refresh_toke", "password", "implicit")
+                .authorities("role_client")
+                .resourceIds("apis")
+                .scopes("read")
+                .secret("secret")
+                .accessTokenValiditySeconds(300);
+    }
+
 
 
 
